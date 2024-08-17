e9fbafc9077b82380990daba51482dd113feec32
davbee <davpih@gmail.com>
2024-08-17 14:55:16 -0700

    make module function maxprofit instead of linear programming

--- maxprofit.py
+++ maxprofit.py
@@ -11,23 +11,37 @@ Date: 2024-08-17
 
 import random
 
-# a list of randomly generated number simulating predicted daily stock prices
-numbers = [random.randint(1, 100) for _ in range(10)]
-print(numbers)
-
 # number = random.choice(numbers)
 # print(number)
 
-profits = []
-for i in range(len(numbers) - 1):
-    # create a new slice
-    newBlock = numbers[i:]
-    # profits = max of the new slide minus the stock market
-    # entry point for the day
-    profit = max(newBlock) - newBlock[0]
-    #  append the daily profit to profits for the overall trading period
-    profits.append(profit)
 
-# get max profit from the profits for the entire trading period
-maxProfit = max(profits)
-print(maxProfit)
+def maxprofit(numbers: list) -> int:
+    """
+    calculate the maximum profit that could be generated
+    from the prediction
+    """
+    profits = []
+    for i in range(len(numbers) - 1):
+        # create a new slice
+        newblock = numbers[i:]
+
+        # profits = max of the new slide minus the stock market
+        # entry point for the day
+        profit = max(newblock) - newblock[0]
+
+        #  append the daily profit to profits for the overall trading period
+        profits.append(profit)
+
+    # get max profit from the profits for the entire trading period
+    peakprofit = max(profits)
+
+    return peakprofit
+
+
+if __name__ == "__main__":
+    # a list of randomly generated number simulating
+    # predicted daily stock prices
+    prices = [random.randint(1, 100) for _ in range(10)]
+    print(prices)
+    maxProfit = maxprofit(prices)
+    print(maxProfit)
